d586c567b171cca95ec5cd7cf426dc8b00cf52fd
yinqianhui1990 <645823457@qq.com>
2018-05-30 11:36:59 +0800

    js全屏显示控制

---
+++ activemq2/src/test/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @Author: yinqianhui
+ * @Date Created by ThinkPad on 2018/5/30.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
